f2fe660cedfe41913e339c6b3ff79079509c6770
Jonas Wolfram <jonaswolfram@Jonass-MacBook-Air.local>
2022-04-05 16:21:42 +0700

    Added identicon for login state

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,11 @@
-import React, { useContext } from "react";
-import { ReactComponent as LogoWithText } from "../../assets/icons/logo-with-text.svg";
-import Button from "../button";
-import { ReactComponent as KeyIcon } from "../../assets/icons/key-icon.svg";
-import { Web3Context } from "../../App";
+import React, { useContext } from 'react';
+import { ReactComponent as LogoWithText } from '../../assets/icons/logo-with-text.svg';
+import Button from '../button';
+import { ReactComponent as KeyIcon } from '../../assets/icons/key-icon.svg';
+import { ReactComponent as ReplaceIcon } from '../../assets/icons/replace-icon.svg';
+import { ReactComponent as MetaMaskIcon } from '../../assets/icons/metamask-icon.svg';
+import { Web3Context } from '../../App';
+import * as blockies from 'ethereum-blockies';
 
 const Header = () => {
   const {
@@ -21,9 +24,9 @@ const Header = () => {
       return (
         <Button
           label={
-            isInstallingMetamask ? "Installing Metamask..." : "Install MetaMask"
+            isInstallingMetamask ? 'Installing Metamask...' : 'Install MetaMask'
           }
-          Icon={KeyIcon}
+          Icon={MetaMaskIcon}
           type="inverted-secondary"
           onClick={onClickInstallMetamaskButton}
           isDisabled={isInstallingMetamask}
@@ -44,18 +47,31 @@ const Header = () => {
       return (
         <Button
           label="Change chain"
-          Icon={KeyIcon}
+          Icon={ReplaceIcon}
           type="inverted-secondary"
           onClick={onClickChangeChainButton}
         />
       );
     }
-    return <p>connected {account}</p>;
+    const identicon = blockies.create({ seed: account, size: 10 }).toDataURL();
+    return (
+      <div className="flex items-center gap-4">
+        <p className="font-secondary font-bold text-secondary text-xs tracking-widest uppercase leading-none">
+          Connected
+        </p>
+        <img
+          src={identicon}
+          alt=""
+          title={account}
+          className="w-10 h-10 rounded-full"
+        />
+      </div>
+    );
   };
 
   return (
-    <header className="fixed top inset-x-0 z-10 bg-secondary shadow-2xl">
-      <nav className="container flex justify-between items-center py-3">
+    <header className="fixed top inset-x-0 z-10 bg-secondary shadow-2xl h-16">
+      <nav className="container flex justify-between items-center h-full">
         <LogoWithText />
         {hasInitialisedWeb3 && renderHeaderContent()}
       </nav>
